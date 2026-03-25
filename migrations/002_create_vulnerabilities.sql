50c6ca793566eca6144a188cbc717f0ecc297c9a
Edmund Leemhuis <edmundleemhuis@gmail.com>
2026-03-25 00:11:59 -0700

    remove extra files

--- migrations/002_create_vulnerabilities.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS vulnerabilities (
-    id         SERIAL      PRIMARY KEY,
-    cve_id     TEXT        NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-    CONSTRAINT uq_vulnerabilities_cve_id UNIQUE (cve_id)
-);
